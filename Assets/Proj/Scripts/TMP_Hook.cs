db0bb39addf38e1ecae1bac92a7ded7c2fb2bc3f
matsuzaka@canmu.jp <Yyiz67w86FuzgXiP>
2025-11-04 18:13:37 +0900

    initial commit,

---
+++ Assets/Proj/Scripts/TMP_Hook.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class TMP_Hook : TMPro.TextMeshPro
+{
+    [SerializeField]
+    private ComputeShader GenerateMeshShader;
+
+    protected override void OnValidate()
+    {
+        base.OnValidate();
+    }
+}
